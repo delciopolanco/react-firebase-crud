5c2163d411fd3beadc92a31680fa638945a1bc3d
-.- <delciopolanco@gmail.com>
2021-11-25 13:06:43 -0400

    adding skeleton

--- src/components/product-list-item/Product-List-Item.js
+++ src/components/product-list-item/Product-List-Item.js
@@ -1,5 +1,23 @@
-const ProductListItem = ({ name, picture }) => {
-  console.log('ProductListItem RENDERIZADO');
+const ItemSkeleton = () => {
+  return (
+    <div className="animate-pulse flex flex-col mb-8 w-2/6">
+      <button className="block flex flex-col p-5 relative no-underline pointer rounded-md" style={{ flex: '1 0 auto', color: '#244983' }}>
+        <div className="bg-gray-300 flex flex-wrap flex-row items-center justify-center relative mb-8 p-2 w-full" style={{ flex: '1 0 340px', maxHeight: '340px' }}>
+        </div>
+        <div className="text-2xl text-left font-bold bg-gray-300 w-3/6 h-6" style={{ color: '#244983', margin: '0 0 0.5em' }}></div>
+        <div className="flex flex-col w-full" style={{ flex: '1 0 auto' }}>
+          <div className="hidden">
+          </div>
+          <div className="justify-end">
+            <div className="p-3 inline-block w-full border-none text-sm text-white uppercase text-center bg-gray-300">VER DETALLES</div>
+          </div>
+        </div>
+      </button>
+    </div>
+  )
+}
+
+const Item = (name, picture) => {
   return (
     <div className="flex flex-col mb-8 w-2/6">
       <button className="block flex flex-col p-5 relative no-underline pointer hover:shadow-2xl rounded-md" style={{ flex: '1 0 auto', color: '#244983' }}>
@@ -17,6 +35,10 @@ const ProductListItem = ({ name, picture }) => {
       </button>
     </div>
   )
+}
+
+const ProductListItem = ({ name, picture, isLoading }) => {
+  return <>{isLoading ? ItemSkeleton() : Item(name, picture)}</>
 }
 
 export { ProductListItem };
--- src/containers/Product-List/Product-List.js
+++ src/containers/Product-List/Product-List.js
@@ -1,16 +1,42 @@
 
 import { ProductListItem } from '../../components';
 
-const ProductList = ({ name, items }) => {
- 
+
+const Items = (name, items) => {
   return (
-    <div className="block w-full my-8">
+    <>
       {
         items && items.length ?
           <div className="flex flex-wrap ">
             <h3 className="text-4xl" style={{ flex: '1 100%' }}>{name}</h3>
             {items.map((item, key) => <ProductListItem key={key} name={item.name} picture={item.pictures[0]} />)}
           </div> : null
+      }
+    </>
+  )
+}
+
+const ItemSkeletons = (name) => {
+  return (
+    <>
+      {
+        <div className="flex flex-wrap ">
+          <div style={{ flex: '1 100%' }}>
+            <span className="block animate-pulse font-bold bg-gray-300 w-1/5 h-6"></span>
+          </div>
+          {[1, 1, 1].map((_, key) => <ProductListItem key={key} isLoading={true} />)}
+        </div>
+      }
+    </>
+  )
+}
+
+const ProductList = ({ name, items, isLoading }) => {
+
+  return (
+    <div className="block w-full my-8">
+      {
+        isLoading ? ItemSkeletons(name) : Items(name, items)
       }
     </div>
   )
